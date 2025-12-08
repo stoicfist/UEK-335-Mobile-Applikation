b86b92643693d61d86e03f2e2a308083f335423b
Peter Ngo <stoic.fist@gmail.com>
2025-12-08 10:59:50 +0100

    chore: git init mobile application project

---
+++ MotoTrack/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MotoTrack',
+  webDir: 'www'
+};
+
+export default config;
