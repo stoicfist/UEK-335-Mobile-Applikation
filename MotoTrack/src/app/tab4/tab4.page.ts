01dc37b8baf2568c45e18751aa2c74c4f52962c1
Peter Ngo <stoic.fist@gmail.com>
2025-12-12 09:14:47 +0100

    feat(tab4): enhancing setting tab with system status, about the app, device interface

--- MotoTrack/src/app/tab4/tab4.page.ts
+++ MotoTrack/src/app/tab4/tab4.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonListHeader, IonLabel, IonItem, IonToggle } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonListHeader, IonLabel, IonItem, IonToggle, IonIcon, IonNote, IonBadge } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { ThemeService } from '../services/theme.service';
+import { NetworkService } from '../services/network.service';
 
 @Component({
   selector: 'app-tab4',
@@ -18,12 +19,16 @@ import { ThemeService } from '../services/theme.service';
     IonLabel,
     IonItem,
     IonToggle,
+    IonIcon,
+    IonNote,
+    IonBadge,
   ],
 })
 export class Tab4Page {
   darkMode$ = this.themeService.darkMode$;
+  isOnline$ = this.networkService.isOnline$;
 
-  constructor(private themeService: ThemeService) {}
+  constructor(private themeService: ThemeService, private networkService: NetworkService) {}
 
   onDarkModeToggle(event: any): void {
     this.themeService.toggleDarkMode();
